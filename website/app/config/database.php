141052a8e93cd2c84050c71552f8a95825888218
Katie Paxton-Fear <K.Paxton-Fear@edu.salford.ac.uk>
2016-01-20 14:00:18 +0000

    Updating Composer
    Adding IDE Helper and generate commands

--- website/app/config/database.php
+++ website/app/config/database.php
@@ -55,9 +55,9 @@ return array(
 		'mysql' => array(
 			'driver'    => 'mysql',
 			'host'      => 'localhost',
-			'database'  => 'forge',
-			'username'  => 'forge',
-			'password'  => '',
+			'database'  => 'spat',
+			'username'  => 'root',
+			'password'  => 'katie',
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
