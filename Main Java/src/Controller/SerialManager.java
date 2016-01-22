d7e219269c29ffe73fbb94867b17045113e2cf28
jimj316 <jimj316@gmail.com>
2016-01-22 11:18:51 +0000

    Added some code that got lost in the port from SerialTest

--- Main Java/src/Controller/SerialManager.java
+++ Main Java/src/Controller/SerialManager.java
@@ -13,123 +13,117 @@ import jssc.SerialPortEventListener;
 import jssc.SerialPortException;
 import jssc.SerialPortList;
 
-
 public class SerialManager implements SerialPortEventListener {
-    
-    String stringPort = null;
-    int portSpeed = 115200;
-    int portDataBits = 8;
-    int portStopBits = 1;
-    String portParity = "PARITY_NONE";
-    SerialPort port = new SerialPort(getPort());
-    StringBuffer buffer = new StringBuffer();
-    
-    public static Set<String> listPorts()
-    {
-	String[] portNames = SerialPortList.getPortNames();
-	Set<String> ret = new HashSet<>();
-	ret.addAll(Arrays.asList(portNames));
-	return ret;
-    }
-    
-    public void setPort(String stringPort){
-	this.stringPort = stringPort;
-    }
-    
-    public String getPort(){
-	return stringPort;
-    }
-    
-    public void setPortSpeed(int portSpeed ){
-	this.portSpeed  = portSpeed ;
-    }
-    
-    public int getPortSpeed(){
-	return portSpeed ;
-    }
-    
-    public void setPortDataBits(int portDataBits ){
-	this.portDataBits  = portDataBits ;
-    }
-    
-    public int getPortDataBits(){
-	return portDataBits ;
-    }
-    
-    public void setPortStopBits(int portStopBits ){
-	this.portStopBits  = portStopBits ;
-    }
-    
-    public int getPortStopBits(){
-	return portDataBits ;
-    }
-    
-    public void setPortParity(String portParity){
-	this.portParity = portParity;
-    }
-    
-    public String getPortParity(){
-	return portParity;
-    }
 
-    public void serialEvent(SerialPortEvent serialPortEvent)
-    {
-	if (serialPortEvent.getEventType() == SerialPortEvent.RXCHAR && stringPort != null)
-	{
-	    try
-	    {
-		final String read = port.readString();
-		if (read != null)
-		{
-		    buffer.append(read);
-		    for (int i = 0; i < buffer.length(); i++)
-		    {
-			char c = buffer.charAt(i);
-			if (c == '\n')
-			{
-			    String out = buffer.substring(0, i);
-			    buffer.delete(0, i + 1);
-			    Data.parseData(out);
-			}
-		    }
-		}
-	    } catch (SerialPortException ex)
-	    {
-		Logger.getLogger(SerialManager.class.getName()).log(Level.SEVERE, null, ex);
-	    }
+	String portNumber = "COM3";
+	int portSpeed = 115200;
+	int portDataBits = 8;
+	int portStopBits = 1;
+	int portParity = SerialPort.PARITY_NONE;
+	SerialPort port = null;
+	StringBuffer buffer = new StringBuffer();
 
+	public static Set<String> listPorts() {
+		String[] portNames = SerialPortList.getPortNames();
+		Set<String> ret = new HashSet<>();
+		ret.addAll(Arrays.asList(portNames));
+		return ret;
 	}
+	
+	public SerialManager()
+    {
+		 init();
     }
-    
-//    public void parse(String message)
-//    {
-////	System.out.println("Message: " + toParse);
-//	String[] split = message.split(",");
-//	int nodeID = Integer.parseInt(split[0]);
-//	String sensorType = split[1];
-//	String sensorName = split[2];
-//	double airTemp = 0.0;
-//	double surfaceTemp = 0.0;
-//
-//	System.out.println("Node ID: " + nodeID);
-//	System.out.println("\tSensor Type: " + sensorType);
-//	System.out.println("\tSensor Name: " + sensorName);
-//
-//	if (sensorType.equals("HFT"))
-//	{
-//	    double heatFlux = Double.parseDouble(split[3]);
-//	    airTemp = Double.parseDouble(split[4]);
-//	    surfaceTemp = Double.parseDouble(split[5]);
-//	    System.out.println("\tHeat Flux: " + heatFlux);
-//	} else if (sensorType.equals("Temp"))
-//	{
-//	    airTemp = Double.parseDouble(split[3]);
-//	    surfaceTemp = Double.parseDouble(split[4]);
-//	}
-//	System.out.println("\tAir Temperature: " + airTemp);
-//	System.out.println("\tSurface Temperature: " + surfaceTemp);
-//    }
-    
-    
+
+	public void init()
+	{
+		try
+		{
+			if (port != null && port.getPortName() != portNumber)
+				port.closePort();
+			if (port == null || port.getPortName() != portNumber)
+			{
+				port = new SerialPort(portNumber);
+				port.openPort();
+				port.addEventListener(this);
+			}
+			port.setParams(portSpeed, portDataBits, portStopBits, portParity);
+			port.setFlowControlMode(SerialPort.FLOWCONTROL_XONXOFF_IN | SerialPort.FLOWCONTROL_XONXOFF_OUT);
+		}
+		catch (SerialPortException ex)
+		{
+			System.err.println("Exception while initialising serial port: " + ex.getMessage());
+		}
+	}
+
+	public void setPort(String stringPort) {
+		this.portNumber = stringPort;
+		init();
+	}
+
+	public String getPort() {
+		return portNumber;
+	}
+
+	public void setPortSpeed(int portSpeed) {
+		this.portSpeed = portSpeed;
+		init();
+	}
+
+	public int getPortSpeed() {
+		return portSpeed;
+	}
+
+	public void setPortDataBits(int portDataBits) {
+		this.portDataBits = portDataBits;
+		init();
+	}
+
+	public int getPortDataBits() {
+		return portDataBits;
+	}
+
+	public void setPortStopBits(int portStopBits) {
+		this.portStopBits = portStopBits;
+		init();
+	}
+
+	public int getPortStopBits() {
+		return portDataBits;
+	}
+
+	public void setPortParity(int portParity) {
+		this.portParity = portParity;
+		init();
+	}
+
+	public int getPortParity() {
+		return portParity;
+	}
+
+	public void serialEvent(SerialPortEvent serialPortEvent) {
+		if (serialPortEvent.getEventType() == SerialPortEvent.RXCHAR
+				&& portNumber != null) {
+			try {
+				final String read = port.readString();
+				if (read != null) {
+					buffer.append(read);
+					for (int i = 0; i < buffer.length(); i++) {
+						char c = buffer.charAt(i);
+						if (c == '\n') {
+							String out = buffer.substring(0, i);
+							buffer.delete(0, i + 1);
+							Data.parseData(out);
+						}
+					}
+				}
+			} catch (SerialPortException ex) {
+				Logger.getLogger(SerialManager.class.getName()).log(
+						Level.SEVERE, null, ex);
+			}
+
+		}
+	}
+
 
 }
