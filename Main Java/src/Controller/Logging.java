c70abd962824973c24606c9d7aa7f67c111e9dfd
Lewis Campbell <eccentaur@outlook.com>
2016-01-27 12:14:33 +0000

    Logging with file Writer

--- Main Java/src/Controller/Logging.java
+++ Main Java/src/Controller/Logging.java
@@ -1,11 +1,24 @@
 package Controller;
 
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class Logging {
 	public Logging() {
 		
 	}
 	
 	public void newRawData(String rawData) {
-		
+		try
+		{
+		    String filename= "LogDate.txt";
+		    FileWriter fw = new FileWriter(filename,true);
+		    fw.write(rawData + "\n");
+		    fw.close();
+		}
+		catch(IOException ioe)
+		{
+		    System.err.println("IOException: " + ioe.getMessage());
+		}
 	}
 }
